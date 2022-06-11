4ef70c0e8035908b18bf393dc2746ff7371b2b9e
Amindu Ishara <aminduishara@gmail.com>
2022-06-11 13:48:17 +0530

    Initial Commit with project setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Reservation from "./components/Reservation";
 
+const navClassName = "nav-link";
+const navActiveClassName = "nav-link active";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink to="/" className={({ isActive }) =>
+                    isActive ? navActiveClassName : navClassName
+                  }>Home</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/reservation" className={({ isActive }) =>
+                    isActive ? navActiveClassName : navClassName
+                  }>Rooms</NavLink>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <div class="input-group">
+                  <input className="form-control custom_search" type="search" placeholder="Search" aria-label="Search" />
+                  <span class="input-group-text custom_icon" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"></i></span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </nav>
+        <div className="container pt-2">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="reservation" element={<Reservation />} />
+          </Routes>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to the homepage!</h2>
+            <p>You can do this, I believe in you.</p>
+        </div>
+    )
+}
+
+export default Home
