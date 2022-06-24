89cd59eae00c9a327bce6e6a9428862abcfd8d39
Maduranga91 <maduranga.cmaduranga.chirath@gmail.com>
2022-06-24 21:26:32 +0530

    Created and Designed new Page Room Details

--- src/pages/RoomDetails/RoomDetails.js
+++ src/pages/RoomDetails/RoomDetails.js
@@ -1,8 +1,30 @@
+import './RoomDetails.css';
+import img1 from '../../Assets/imgs/img1.jpg'
+import img2 from '../../Assets/imgs/img2.jpg'
+import img3 from '../../Assets/imgs/img3.jpg'
+import img4 from '../../Assets/imgs/img4.jpg'
+import img5 from '../../Assets/imgs/img5.jpg'
+
 const RoomDetails = () => {
     return (
         <div>
-            <h1 className="text-success">RoomDetails</h1>
+            <div className="col-md-12 row">
+                <div className="col-md-6 row">
+                    <img src={img1} className="col-md-12 img__cl1" />
+                </div>
+                <div className="col-md-6 row">
+                    <img src={img2} className="col-md-6 img__cl2" />
+                    <img src={img3} className="col-md-6 img__cl2" />
+                    <img src={img4} className="col-md-6 img__cl2" />
+                    <img src={img5} className="col-md-6 img__cl2" />
+                </div>
+            </div>
+
+            <div className="col-md-12 text-left">
+                <p className="p__title">Superior Room</p>
+            </div>
+
         </div>
-    )
+    );
 }
 export default RoomDetails
