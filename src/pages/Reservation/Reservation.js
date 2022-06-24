2300e52c60940a43ecd70a62d0611d102e96f5b4
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-06-25 02:01:30 +0530

    Add the BookingDetail page and Edit the App.js

---
+++ src/pages/Reservation/Reservation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import BookingDetailCmp from '../../components/BookingDetail/BookingDetailCmp'
+import BeachImg from './beach.jpg'
+import './Reservation.css'
+
+function Reservation() {
+    return (
+        <div>
+            <div className='reservation__img'>
+                Hello EveryOne
+            </div>
+            <BookingDetailCmp/>
+        </div>        
+    )
+}
+
+export default Reservation
