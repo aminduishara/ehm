ff437d860d3b2918d52ee32d84b44033cf6f424a
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-06-27 04:28:58 +0530

    Add the elements for reservation page

--- src/pages/Reservation/Reservation.js
+++ src/pages/Reservation/Reservation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BookingDetailCmp from '../../components/BookingDetail/BookingDetailCmp'
 import BeachImg from './beach.jpg'
 import './Reservation.css'
+import TotalCmp from '../../components/Total/TotalCmp'
 
 function Reservation() {
     return (
@@ -10,6 +11,25 @@ function Reservation() {
                 Hello EveryOne
             </div>
             <BookingDetailCmp/>
+            <TotalCmp/>
+            <br></br><br></br><br></br>
+
+            <div className="col-8 col-md-12 col-sm-8">
+                    <div className="row d-flex align-items-center">
+
+                    <div className="col-sm-3 h2 font-weight-bold">
+                        Special<br/>Request For<br/> Your <br/>Reservation
+                    </div>
+                    <div className="col-sm-3 font-weight-bold">
+                        <textarea type="text-area" className="form-control" style={{width:'500px', height:'250px'}}x>Please explain your request: arrival tie, flight details, food, preferences, membership number ....</textarea>
+                    </div>
+                    <div className="col-12 text-end">
+                        <button type="submit" className='btn btn-primary reservation__confirmbtn'>Confirm</button>
+                    </div>
+                    
+                    </div>
+            </div>
+
         </div>        
     )
 }
